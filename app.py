3d27da23bbe36e51ab160af2f3f20536dba9ab3d
Mahboob Elahi <mahboobelahi93@gmail.com>
2022-01-18 10:18:07 +0200

    unsed imports were removed.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # Import libraries
-import threading, time, requests,json,csv,Utility_FUNC
-from flask import Flask, request
+import threading,Utility_FUNC
+from flask import Flask
 import configurations as CONFIG
 
 
@@ -11,14 +11,17 @@ def welcom():
 
     return f"<h1>Hello from {CONFIG.DEVICE_NAME}, listening at {CONFIG.LocIP}:{CONFIG.LocPort}<h1>"
 
+
 @app.route('/simulate', methods= ['GET'])
 def simulateData():
     id= CONFIG.DEVICE_ID
-    url= '127.0.0.1'
+    url= '127.0.0.1:5000'
     thread = threading.Timer(.1, Utility_FUNC.publish_measurements,args=(id,url))
     thread.daemon=True
     thread.start()
     return 'OK'
+
+
 
 
 
